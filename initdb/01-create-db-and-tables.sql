1ef9654021a2f0395309756203f3fe626a095be7
Amogh K Umesh <amogh.k.umesh@gmail.com>
2025-10-13 15:59:42 +0530

    uploading files

---
+++ initdb/01-create-db-and-tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS users (
+    UserID SERIAL PRIMARY KEY,
+    UserName VARCHAR(50) UNIQUE NOT NULL,
+    UserPassword VARCHAR(100) NOT NULL,
+    UserType VARCHAR(20) NOT NULL DEFAULT 'bidder',
+    Classification VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS bookings(
+    BookingID SERIAL PRIMARY KEY,
+    BidderName varchar(40) NOT NULL,
+    WorkerName varchar(40) NOT NULL,
+    StartDate DATE not null,
+    EndDate DATE not null,
+    HoursBooked INTEGER[][],
+    HourlyRate INT not null
+);
+
+CREATE TABLE IF NOT EXISTS availability(
+    AvailabilityID SERIAL PRIMARY KEY,
+    WorkerName VARCHAR(40) NOT NULL,
+    AvailableHours INTEGER[][]
+);
